ae60b98ed4ff8464bb242766053689d7c9adc4ce
kuccho524 <kucchachachanocha@gmail.com>
2021-10-16 14:06:06 +0900

    exercise4

--- exercise4/exercise4-3.sql
+++ exercise4/exercise4-3.sql
@@ -1,5 +1,6 @@
-shohin_id, shohin_mei, hanbai_tanka, shiire_tanka, saekiのカラムを持つShohinSaekiテーブルを作成し、
+/*shohin_id, shohin_mei, hanbai_tanka, shiire_tanka, saekiのカラムを持つShohinSaekiテーブルを作成し、
 ShohinSaekiテーブルにShohinテーブルの内容をコピーする。
 この時、saekiにhanbai_tankaとshiire_tankaの差分を挿入する
+*/
 
 INSERT INTO S_Saeki (shohin_id, shohin_mei, hanbai_tanka, shiire_tanka, saeki) SELECT shohin_id, shohin_mei, hanbai_tanka, shiire_tanka, hanbai_tanka - shiire_tanka FROM Shohin;
